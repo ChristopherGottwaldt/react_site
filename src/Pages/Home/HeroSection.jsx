0da6589e2c696021c56cb17faa7affdd966c53be
Chris Gottwaldt <cgottwaldt@wisc.edu>
2023-12-30 03:14:00 -0600

    Add basic website layout for homepage

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,20 @@
+export default function HeroSection() {
+    return (
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section--content">
+                    <p className="section--title">Hi, I'm Chris!</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color">Full Stack</span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                        test test test test test
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get In Touch</button>
+            </div>
+        </section>
+    )
+}
